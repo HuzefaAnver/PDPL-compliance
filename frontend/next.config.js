97d6c139b7b0ed0fc2ce42c4be46186f2d5d4d43
huzefa <huzefaanver@gmail.com>
2026-04-05 21:19:47 +0500

    chore: remove experimental SWC flag

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
-    // Force disable native SWC binaries and use WASM fallback for WebContainer compatibility
-    experimental: {
-        forceSwcTransforms: true,
-    }
 };
 
 module.exports = nextConfig;
